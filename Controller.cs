ad77a8da4daac06c0f83d367c43a478af7436cd1
Ryan Cooper <Cooper.ryan0942@gmail.com>
2021-02-11 19:30:16 +0000

    Added missing controller class. Seems there was a problem with the upload to github.

---
+++ Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database_Test
+{
+    class Controller
+    {
+        DatabaseController dbc;
+        View v;
+        public Controller()
+        {
+            dbc = new DatabaseController();
+            v = new View();
+        }
+
+        public void displayCustomerDetails()
+        {
+            List<Customer> customers = dbc.GetCustomers();
+            v.CustomerDetails(customers);
+        }
+    }
+}
